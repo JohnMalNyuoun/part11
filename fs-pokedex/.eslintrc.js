4703185513ddfd23328f069779610dcf760a6764
johnmalnyuon <wmal44884@gmail.com>
2026-09-10 14:33:15 +0300

    10.6 Fix the code

--- fs-pokedex/.eslintrc.js
+++ fs-pokedex/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -21,12 +22,9 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
+      2
     ],
+    "linebreak-style": "off",
     "quotes": [
       "error",
       "single"
@@ -43,7 +41,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
