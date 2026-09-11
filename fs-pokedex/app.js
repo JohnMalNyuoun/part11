4dd8319ec268d292c87a8d39ebc0c61351aa7857
johnmalnyuon <wmal44884@gmail.com>
2026-09-11 16:56:11 +0300

    Add /health endpoint for Render health checks

--- fs-pokedex/app.js
+++ fs-pokedex/app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable or fallback to 5001
+
 const PORT = process.env.PORT || 5001
 
-// SERVE FROM 'dist' INSTEAD OF 'build'
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
