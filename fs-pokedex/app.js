40dd0770f72fd633a6774e9b762ac5bc4717d735
johnmalnyuon <wmal44884@gmail.com>
2026-09-11 16:06:16 +0300

    git commit -m "Configure Express static build serving from dist"

--- fs-pokedex/app.js
+++ fs-pokedex/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable
+// get the port from env variable or fallback to 5001
 const PORT = process.env.PORT || 5001
 
+// SERVE FROM 'dist' INSTEAD OF 'build'
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`server started on port ${PORT}`)
 })
