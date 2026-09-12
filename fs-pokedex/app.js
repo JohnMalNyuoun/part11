f0191a0d92b949d88cb723567bd56be629c9a586
johnmalnyuon <wmal44884@gmail.com>
2026-09-12 09:12:53 +0300

    10.12 Restore healthy /health endpoint

--- fs-pokedex/app.js
+++ fs-pokedex/app.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
 
-
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error('Simulated deployment failure')
   res.send('ok')
 })
 
